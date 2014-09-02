84345f7e8229001890c962ef5c21076cd1545681
Esteban Barrios <ingestebanbarrios@gmail.com>
2014-09-02 00:43:45 -0500

    example 2 class 3

--- clase3/examples/example2/js/index.js
+++ clase3/examples/example2/js/index.js
@@ -2,19 +2,10 @@ var dibujo, lienzo;
 function inicio (){
     dibujo = document.getElementById("dibujito");
     lienzo = dibujo.getContext("2d");
+
+    dibujarMariposa(lienzo);
     
-    //hacer un borde dibujando un cuadro
-    lienzo.moveTo(0,0);
-    lienzo.lineTo(300,0);
-    lienzo.lineTo(300,300);
-    lienzo.lineTo(0,300);
-    lienzo.lineTo(0,0);
-    lienzo.stroke();
-    
-    lienzo.strokeStyle = "#F00";
-    lienzo.stroke();
-    
-    //trazo con nuevo color
+    //trazo nuevo
     lienzo.beginPath();
     lienzo.strokeStyle = "#00F";
     //x,y = coordenadas del centro del circulo
@@ -22,7 +13,71 @@ function inicio (){
     lienzo.arc(150,150,100, (Math.PI * 2), false);
     lienzo.closePath();
     lienzo.stroke();
+}
+
+
+function dibujarGrilla(l){
+    var ancho = 300, alto = 300;
+    var linea;
+    var anchoLinea = 30;
+    var limX = ancho / anchoLinea;
+    var limY = alto / anchoLinea;
+    for (linea = 0; linea <= limX; linea ++){
+        l.beginPath();
+        l.strokeStyle = "#AAA";
+        l.moveTo(linea * anchoLinea, 0);
+        l.lineTo(linea * anchoLinea, 300);
+        l.closePath();
+        l.stroke();
+    }
+    for (linea = 0; linea <= limY; linea ++){
+        l.beginPath();
+        l.strokeStyle = "#AAA";
+        l.moveTo(0, linea * anchoLinea);
+        l.lineTo(300, linea * anchoLinea);
+        l.closePath();
+        l.stroke();
+    }
+}
+
+function dibujarMariposa(l){
+    var ancho = 300, alto = 300;
+    var lineas;
+    var anchoLinea = 10;
+    var limX = ancho / anchoLinea;
+    var limY = alto / anchoLinea;
+    for (linea = 0; linea <= limX; linea ++){
+        l.beginPath();
+        l.strokeStyle = "#AAA";
+        l.moveTo(0, linea * 10);
+        l.lineTo(linea * 10, 300);
+        l.closePath();
+        l.stroke();
+    }
+    for (linea = 0; linea <= limY; linea ++){
+        l.beginPath();
+        l.strokeStyle = "#AAA";
+        l.moveTo(linea * 10, 0);
+        l.lineTo(300, linea * 10);
+        l.closePath();
+        l.stroke();
+    }
     
-    
-    console.log(lienzo);
+    for (linea = 0; linea <= limX; linea ++){
+        l.beginPath();
+        l.strokeStyle = "#AAA";
+        l.moveTo(linea * 10, 300);
+        l.lineTo(300, (300 - (linea * 10)));
+        l.closePath();
+        l.stroke();
+    }
+    for (linea = 0; linea <= limX; linea ++){
+        l.beginPath();
+        l.strokeStyle = "#AAA";
+        l.moveTo((300 - (linea * 10)), 0);
+        l.lineTo(0 , (linea * 10));
+        l.closePath();
+        l.stroke();
+    }
+
 }
